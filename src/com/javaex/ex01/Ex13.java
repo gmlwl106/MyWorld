e0d905877bbeccdcbb0926b989d73060dfbc5320
gmlwl106 <66778894@naver.com>
2022-06-26 16:09:57 +0900

    자릿수 더하기

---
+++ src/com/javaex/ex01/Ex13.java
@@ -0,0 +1,22 @@
+package com.javaex.ex01;
+
+/* 자릿수 더하기
+ * 자연수 N이 주어지면, N의 각 자릿수의 합을 구해서 return 하는 solution 함수를 만들어 주세요.
+ * 예를들어 N = 123이면 1 + 2 + 3 = 6을 return 하면 됩니다.
+ */
+public class Ex13 {
+	
+	private static class Solution {
+	    public int solution(int n) {
+	        int answer = 0;
+
+	        String[] num = String.valueOf(n).split("");
+	        
+	        for(int i = 0; i<num.length; i++) {
+	        	answer += Integer.parseInt(num[i]);
+	        }
+
+	        return answer;
+	    }
+	}
+}
