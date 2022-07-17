24d003d9239259046d835d0bbd23535ac38446ca
gmlwl106 <66778894@naver.com>
2022-07-17 21:07:16 +0900

    이상한 문자 만들기

---
+++ src/com/javaex/ex01/Ex14.java
@@ -0,0 +1,18 @@
+package com.javaex.ex01;
+
+/* 이상한 문자 만들기
+ * 문자열 s는 한 개 이상의 단어로 구성되어 있습니다. 각 단어는 하나 이상의 공백문자로 구분되어 있습니다.
+ * 각 단어의 짝수번째 알파벳은 대문자로, 홀수번째 알파벳은 소문자로 바꾼 문자열을 리턴하는 함수, solution을 완성하세요.
+ */
+public class Ex14 {
+
+	public static void main(String[] args) {
+		String s = "try hello world";
+		
+		String[] str = s.split(" ");
+		
+		for(int i=0; i<str.length; i++) {
+			String[] string = str[i].split("");
+		}
+	}
+}
